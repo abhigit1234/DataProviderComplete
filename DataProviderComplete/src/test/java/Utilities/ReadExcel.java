4c72ffa5d8628c15fc924ea24d63d97cb834531f
abhilash <abhilashnaidu21@gmail.com>
2024-11-14 16:24:12 +0530

    n

--- DataProviderComplete/src/test/java/Utilities/ReadExcel.java
+++ DataProviderComplete/src/test/java/Utilities/ReadExcel.java
@@ -12,56 +12,40 @@ import org.testng.annotations.Test;
 
 public class ReadExcel {
 
-	@DataProvider(name = "a",parallel = true)
-	public Object[][] rules()throws Exception{
-	FileInputStream fis = new FileInputStream("C:\\\\Users\\\\abhil\\\\OneDrive\\\\Desktop\\\\test.xlsx");
-	XSSFWorkbook wb = new XSSFWorkbook(fis);
-	XSSFSheet sheet =	wb.getSheet("TestCase");
-	int row = sheet.getLastRowNum();
-	int cel = sheet.getRow(0).getLastCellNum();
-	Object[][] data = new Object[row][cel];
-	for (int i = 1; i <= row; i++) {
-		for (int j = 0; j < cel; j++) {
-			XSSFCell cell =	sheet.getRow(i).getCell(j);
-			DataFormatter df = new DataFormatter();
-			data[i-1][j] = df.formatCellValue(cell);
-			
-		}
-	
-	}
-	wb.close();
-	fis.close();
-	return data;
-	
-	}
-	}
-	
-	
-	
-	
-	
-	
-/*	@DataProvider(name = "login",parallel = true)
+	/*
+	 * @DataProvider(name = "a",parallel = true) public Object[][] rules()throws
+	 * Exception{ FileInputStream fis = new
+	 * FileInputStream("C:\\\\Users\\\\abhil\\\\OneDrive\\\\Desktop\\\\test.xlsx");
+	 * XSSFWorkbook wb = new XSSFWorkbook(fis); XSSFSheet sheet =
+	 * wb.getSheet("TestCase"); int row = sheet.getLastRowNum(); int cel =
+	 * sheet.getRow(0).getLastCellNum(); Object[][] data = new Object[row][cel]; for
+	 * (int i = 1; i <= row; i++) { for (int j = 0; j < cel; j++) { XSSFCell cell =
+	 * sheet.getRow(i).getCell(j); DataFormatter df = new DataFormatter();
+	 * data[i-1][j] = df.formatCellValue(cell);
+	 * 
+	 * }
+	 * 
+	 * } wb.close(); fis.close(); return data;
+	 * 
+	 * } }
+	 */
+
+	@DataProvider(name = "login", parallel = true)
 	@Test
 	public Object[][] getData() throws Exception {
-		String path = "C:\\Users\\abhil\\OneDrive\\Desktop\\test.xlsx";	
+		String path = "C:\\Users\\abhil\\OneDrive\\Desktop\\test.xlsx";
 		ExcelUtility xl = new ExcelUtility(path);
 		int totalRows = xl.getRowCount("TestCase");
 		int totalCols = xl.getCellCount("TestCase", 1);
 		Object[][] data = new Object[totalRows][totalCols];
-		for (int i = 1; i <=totalRows; i++) {
+		for (int i = 1; i <= totalRows; i++) {
 			for (int j = 0; j < totalCols; j++) {
-				data[i-1][j] = xl.getCellData("TEstCase", i, j);
+				data[i - 1][j] = xl.getCellData("TEstCase", i, j);
 			}
-			
-		}return data;
-		
-	
-		
+
+		}
+		return data;
+
 	}
-	
-	
-	
-	
+
 }
-	*/
